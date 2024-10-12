473148d169e3d9bdb3b629c8118298504ce69b13
TheDarkElites <73414180+TheDarkElites@users.noreply.github.com>
2024-10-12 16:46:58 -0400

    added comments for owen's sanity

--- Webdownloader.py
+++ Webdownloader.py
@@ -5,27 +5,38 @@ whitelistTags = ["[gb]", "instock"]
 blacklistTags = ["closed", "completed", "complete", "cancelled"]
 
 def scrapePage(url):
+    #loads up index page of forums to scrape through
     savePage(url, "index", "")
     f = open("index.html", "r")
+    #Creates soup
     indexPage = BeautifulSoup(f, "html.parser")
+    #Finds all td divs and iterates through
     for post in indexPage.find_all("td"):
+        #throw out any results that dont contain the post link
         if post.get('class')[0] != "subject" or post.get('class')[1] != "windowbg2":
             continue
+        #Go through a divs (for link)
         for postA in post.find_all("a"):
+            #gets the actual link to the keyboard post
             link = postA.get("href")
+            #filter out links that arent to the actual post (profile links)
             if str(link).find("profile") != -1:
                 continue
+            #get the link text
             text = postA.contents[0]
             text = text.lower()
+            #throw out links that contain blacklisted terms
             for tag in blacklistTags:
                 if text.find(tag) != -1:
                     continue
+            #throw out links that contain no whitelisted terms
             tag_found = False
             for tag in whitelistTags:
                 if text.find(tag) != -1:
                     tag_found = True
             if not tag_found:
                 continue
+            #save page using savePage
             try: 
                 savePage(link)
             except:
